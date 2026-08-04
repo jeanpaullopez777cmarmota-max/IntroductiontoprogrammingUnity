2dc9568b036dd6507be000e3db418d0fd2f601a4
jeanpaullopez777cmarmota-max <jeanpaullopez777cmarmota@gmail.com>
2026-08-04 11:24:59 -0500

    Variables cuarta clase

--- Myveryfirstproject/Assets/_Main/Scripts/PlayerMovement.cs
+++ Myveryfirstproject/Assets/_Main/Scripts/PlayerMovement.cs
@@ -1,27 +1,33 @@
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour //this class denotes where we'll carry out all changes to our current script
 {
     // variables
-    
-
-    public float _fuerza = 2000f;
-
-    [SerializeField] private Rigidbody2D _rigidbody2D;
 
 
+    public float height = 1.84f;
+    public int edad = 30;
+    public string nombre = "Roberto";
+    public bool cantheyvote = true;
 
+    public GameObject gameObject;
+    public Rigidbody2D rigidbody2D; //responsible for game physics
+    public Collider2D collider2D;  //does it have collision? 
+    public SpriteRenderer spriteRenderer;
+    public Transform transformoormove;
 
-    // Start is called before the first frame update
-    void Start() //execute only once, it just executes a single frame 
+    private void Start()
     {
-        _rigidbody2D.AddForce(Vector2.right * _fuerza);
-
+        Debug.Log("LEROLERO LERO ELROROROROR: " + nombre + " your age is: " + edad + " let's see if your a manlet: " + height); //to print stuff on console 
+        rigidbody2D.simulated = false; //after the dot you write the property you wanna tweak. 
+        spriteRenderer.color = Color.green; //capital C letter indicative of it being a function
+        transformoormove.position = new Vector3(4f, 0f, 0f); //each number corresponds to xyz respectively. -x and x go from left to right, -y and y signals upward or downward movement and -z and z is 
+                                                              // for movement either forward or backward 
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
     }
 }
+  
