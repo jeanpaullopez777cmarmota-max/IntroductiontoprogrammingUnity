233fda86f21dac069c606beea914cd71952d26c9
jeanpaullopez777cmarmota-max <jeanpaullopez777cmarmota@gmail.com>
2026-08-06 11:35:46 -0500

    flappybird prototype

--- Myveryfirstproject/Assets/_Main/Scripts/PlayerMovement.cs
+++ Myveryfirstproject/Assets/_Main/Scripts/PlayerMovement.cs
@@ -1,33 +1,28 @@
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour //this class denotes where we'll carry out all changes to our current script
+public class PlayerMovement : MonoBehaviour //this class denotes where we'll carry out all changes to our current script. Monobehaviour is used for adding components on unity's interface
 {
     // variables
 
-
-    public float height = 1.84f;
-    public int edad = 30;
-    public string nombre = "Roberto";
-    public bool cantheyvote = true;
-
-    public GameObject gameObject;
-    public Rigidbody2D rigidbody2D; //responsible for game physics
-    public Collider2D collider2D;  //does it have collision? 
-    public SpriteRenderer spriteRenderer;
-    public Transform transformoormove;
+    [SerializeField] private float _verticalforce = 5f;
+    [SerializeField] private float _speed = 5f;
+    [SerializeField] private Rigidbody2D _rigidbody2D;
+    
 
     private void Start()
     {
-        Debug.Log("LEROLERO LERO ELROROROROR: " + nombre + " your age is: " + edad + " let's see if your a manlet: " + height); //to print stuff on console 
-        rigidbody2D.simulated = false; //after the dot you write the property you wanna tweak. 
-        spriteRenderer.color = Color.green; //capital C letter indicative of it being a function
-        transformoormove.position = new Vector3(4f, 0f, 0f); //each number corresponds to xyz respectively. -x and x go from left to right, -y and y signals upward or downward movement and -z and z is 
-                                                              // for movement either forward or backward 
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+
+        
     }
 
     private void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            _rigidbody2D.AddForce(Vector2.up * _verticalforce);
+        }
+        _rigidbody2D.velocity = Vector2.right * _speed * Time.deltaTime;
     }
 }
   
